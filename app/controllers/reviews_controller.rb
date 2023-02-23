660c7ba297845320e54ab2252bee8e230037fe4f
jane eme <janeeme23@gmail.com>
2023-02-23 10:52:01 +0100

    yelp

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,30 @@
+class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: [:index, :new, :create]
+
+  def index
+    @reviews = Review.where(restaurant_id: @restaurant)
+  end
+
+  def new
+    # we need @restaurant in our `simple_form_for`
+    # @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    @review.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.create(name: "Bristol", address: "Bristol Street", phone_number: "01234 578990", category: "chinese")
+Restaurant.create(name: "Berlin", address: "Berlin Street", phone_number: "01434 5768990", category: "italian")
+Restaurant.create(name: "Tokyo", address: "London Street", phone_number: "01334 578990", category: "japanese")
+Restaurant.create(name: "Le Champ", address: "Rudy Street", phone_number: "01534 578990", category: "french")
+Restaurant.create(name: "Pizza Cap", address: "Brine Street", phone_number: "01634 578990", category: "belgian")
